1d1a002c1574ab01a3a09afd721ac5e4ad016c95
scanmme <lit468@163.com>
2019-05-27 09:59:04 +0800

    add modify

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-sudo -s cp vimrc ~/.vimr
+sudo -s cp vimrc ~/.vimrc
 sudo -s cp ctags /usr/bin/
 sudo -s cp cscope /usr/bin/
 sudo -s cp cscope-indexer /usr/bin/
+mkdir ~/.vim
+sudo -s cp doc/ ~/.vim/ -R
+sudo -s cp plugin/ ~/.vim/ -R
 
